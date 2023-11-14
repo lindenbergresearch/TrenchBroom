e43e49423f7675162ccdcd82185b18a628786b02
Patrick Lindenberg <heapdump@icloud.com>
2023-11-14 11:06:57 +0100

    initial commit

---
+++ common/src/View/PreferencesPreferencePane.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "View/PreferencePane.h"
+#include "View/PreferenceModel.h"
+
+
+class QWidget;
+
+class QTableView;
+
+class QSortFilterProxyModel;
+
+namespace TrenchBroom::View {
+class ColorModel;
+
+class PreferencesPreferencePane : public PreferencePane {
+Q_OBJECT
+private:
+    QTableView *m_table;
+    PreferenceModel *m_model;
+    QSortFilterProxyModel *m_proxy;
+    QLabel *infoLabel;
+    QPushButton *restoreDefaultButton;
+    QPushButton *setValueButton;
+
+public:
+    explicit PreferencesPreferencePane(QWidget *parent = nullptr);
+
+private:
+    bool doCanResetToDefaults() override;
+
+    void doResetToDefaults() override;
+
+    void doUpdateControls() override;
+
+    bool doValidate() override;
+
+    void updateLabel();
+
+    QHBoxLayout *getControlButtonLayout();
+};
+} // namespace TrenchBroom::View
