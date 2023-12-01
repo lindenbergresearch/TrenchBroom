a266dbcfc32b23b408f89d5e4f585aaa053271b3
Patrick Lindenberg <heapdump@icloud.com>
2023-12-01 11:10:10 +0100

    add optional object bounds render add line-width for object bounds add configurable units digits size of labels

--- common/src/Renderer/SelectionBoundsRenderer.cpp
+++ common/src/Renderer/SelectionBoundsRenderer.cpp
@@ -46,7 +46,9 @@ private:
     const Camera &m_camera;
 
 public:
-    SizeTextAnchor2D(const vm::bbox3 &bounds, const vm::axis::type axis, const Renderer::Camera &camera) : m_bounds(bounds), m_axis(axis), m_camera(camera) {
+    SizeTextAnchor2D(const vm::bbox3 &bounds, const vm::axis::type axis, const Renderer::Camera &camera) : m_bounds(
+        bounds
+    ), m_axis(axis), m_camera(camera) {
     }
 
 private:
@@ -60,11 +62,9 @@ private:
     TextAlignment::Type alignment() const override {
         if (m_axis == vm::axis::x) {
             return TextAlignment::Top;
-        }
-        else if (m_axis == vm::axis::y && m_camera.direction().x() != 0.0f) {
+        } else if (m_axis == vm::axis::y && m_camera.direction().x() != 0.0f) {
             return TextAlignment::Top;
-        }
-        else {
+        } else {
             return TextAlignment::Right;
         }
     }
@@ -94,7 +94,9 @@ private:
     const Camera &m_camera;
 
 public:
-    SizeTextAnchor3D(const vm::bbox3 &bounds, const vm::axis::type axis, const Renderer::Camera &camera) : m_bounds(bounds), m_axis(axis), m_camera(camera) {
+    SizeTextAnchor3D(const vm::bbox3 &bounds, const vm::axis::type axis, const Renderer::Camera &camera) : m_bounds(
+        bounds
+    ), m_axis(axis), m_camera(camera) {
     }
 
 private:
@@ -109,96 +111,73 @@ private:
                 (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::within)) {
                 pos[0] = m_bounds.min.x();
                 pos[1] = m_bounds.max.y();
-            }
-            else if ((camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) ||
-                     (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater)) {
+            } else if ((camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) ||
+                       (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater)) {
                 pos[0] = m_bounds.max.x();
                 pos[1] = m_bounds.max.y();
-            }
-            else if ((camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) ||
-                     (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within)) {
+            } else if ((camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) ||
+                       (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within)) {
                 pos[0] = m_bounds.max.x();
                 pos[1] = m_bounds.min.y();
-            }
-            else if ((camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) ||
-                     (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less)) {
+            } else if ((camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) ||
+                       (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less)) {
                 pos[0] = m_bounds.min.x();
                 pos[1] = m_bounds.min.y();
-            }
-            else {
+            } else {
                 // X and Y are within
                 pos[0] = camDir.x() <= 0.0f ? m_bounds.min.x() : m_bounds.max.x();
                 pos[1] = camDir.y() <= 0.0f ? m_bounds.min.y() : m_bounds.max.y();
             }
 
             pos[2] = m_bounds.min.z() + half.z();
-        }
-        else {
+        } else {
             if (m_axis == vm::axis::x) {
                 pos[0] = m_bounds.min.x() + half.x();
                 if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::less) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.y() : m_bounds.max.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::within) {
                     pos[1] = m_bounds.max.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.y() : m_bounds.min.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.y() : m_bounds.max.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::within) {
                     pos[1] = camDir.y() <= 0.0f ? m_bounds.min.y() : m_bounds.max.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.y() : m_bounds.min.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.y() : m_bounds.max.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within) {
                     pos[1] = m_bounds.min.y();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) {
                     pos[1] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.y() : m_bounds.min.y();
                 }
-            }
-            else {
+            } else {
                 pos[1] = m_bounds.min.y() + half.y();
                 if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::less) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.x() : m_bounds.max.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::within) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.x() : m_bounds.max.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::less && camPos[1] == vm::bbox3::Range::greater) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.min.x() : m_bounds.max.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::less) {
                     pos[0] = m_bounds.min.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::within) {
                     pos[0] = camDir.x() <= 0.0f ? m_bounds.min.x() : m_bounds.max.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::within && camPos[1] == vm::bbox3::Range::greater) {
                     pos[0] = m_bounds.max.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::less) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.x() : m_bounds.min.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::within) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.x() : m_bounds.min.x();
-                }
-                else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) {
+                } else if (camPos[0] == vm::bbox3::Range::greater && camPos[1] == vm::bbox3::Range::greater) {
                     pos[0] = camPos[2] == vm::bbox3::Range::within ? m_bounds.max.x() : m_bounds.min.x();
                 }
             }
 
             if (camPos[2] == vm::bbox3::Range::less) {
                 pos[2] = m_bounds.min.z();
-            }
-            else {
+            } else {
                 pos[2] = m_bounds.max.z();
             }
         }
@@ -214,8 +193,7 @@ private:
         const auto camPos = m_bounds.relative_position(vm::vec3(m_camera.position()));
         if (camPos[2] == vm::bbox3::Range::less) {
             return TextAlignment::Top;
-        }
-        else {
+        } else {
             return TextAlignment::Bottom;
         }
     }
@@ -245,7 +223,8 @@ private:
     const Renderer::Camera &m_camera;
 
 public:
-    MinMaxTextAnchor3D(const vm::bbox3 &bounds, const vm::bbox3::Corner minMax, const Renderer::Camera &camera) : m_bounds(bounds), m_minMax(minMax),
+    MinMaxTextAnchor3D(const vm::bbox3 &bounds, const vm::bbox3::Corner minMax, const Renderer::Camera &camera)
+        : m_bounds(bounds), m_minMax(minMax),
         m_camera(camera) {
     }
 
@@ -253,8 +232,7 @@ private:
     vm::vec3f basePosition() const override {
         if (m_minMax == vm::bbox3::Corner::min) {
             return vm::vec3f(m_bounds.min);
-        }
-        else {
+        } else {
             return vm::vec3f(m_bounds.max);
         }
     }
@@ -262,17 +240,16 @@ private:
     TextAlignment::Type alignment() const override {
         const auto camPos = m_bounds.relative_position(vm::vec3(m_camera.position()));
         if (m_minMax == vm::bbox3::Corner::min) {
-            if ((camPos[1] == vm::bbox3::Range::less) || (camPos[1] == vm::bbox3::Range::within && camPos[0] != vm::bbox3::Range::less)) {
+            if ((camPos[1] == vm::bbox3::Range::less) ||
+                (camPos[1] == vm::bbox3::Range::within && camPos[0] != vm::bbox3::Range::less)) {
                 return TextAlignment::Top | TextAlignment::Right;
-            }
-            else {
+            } else {
                 return TextAlignment::Top | TextAlignment::Left;
             }
-        }
-        else if ((camPos[1] == vm::bbox3::Range::less) || (camPos[1] == vm::bbox3::Range::within && camPos[0] != vm::bbox3::Range::less)) {
+        } else if ((camPos[1] == vm::bbox3::Range::less) ||
+                   (camPos[1] == vm::bbox3::Range::within && camPos[0] != vm::bbox3::Range::less)) {
             return TextAlignment::Bottom | TextAlignment::Left;
-        }
-        else {
+        } else {
             return TextAlignment::Bottom | TextAlignment::Right;
         }
     }
@@ -295,14 +272,23 @@ SelectionBoundsRenderer::SelectionBoundsRenderer(const vm::bbox3 &bounds) : m_bo
 }
 
 void SelectionBoundsRenderer::render(RenderContext &renderContext, RenderBatch &renderBatch) {
-    // renderBounds(renderContext, renderBatch);
+    bool showObjectBounds = pref(Preferences::ShowObjectBoundsSelectionBounds);
+
+    if (showObjectBounds) {
+        renderBounds(renderContext, renderBatch);
+    }
+
     renderSize(renderContext, renderBatch);
-    // renderMinMax(renderContext, renderBatch);
+
+    if (pref(Preferences::SelectionBoundsShowMinMax)) {
+        renderMinMax(renderContext, renderBatch);
+    }
 }
 
 void SelectionBoundsRenderer::renderBounds(RenderContext &renderContext, RenderBatch &renderBatch) {
     RenderService renderService(renderContext, renderBatch);
     renderService.setForegroundColor(pref(Preferences::SelectionBoundsColor));
+    renderService.setLineWidth(pref(Preferences::SelectionBoundsLineWidth));
     renderService.renderBounds(vm::bbox3f(m_bounds));
 }
 
@@ -316,10 +302,11 @@ void SelectionBoundsRenderer::renderSize(RenderContext &renderContext, RenderBat
 const std::string SelectionBoundsRenderer::getFormattedUnitsString(float value_units) {
     auto unitsDisplayType = (Preferences::UnitsDisplay) pref(Preferences::UnitsDisplayType);
     auto metricConversationFactor = pref(Preferences::MetricConversationFactor);
+    auto unitsMaxDigits = pref(Preferences::UnitsMaxDigits);
     std::stringstream buffer;
 
-    auto units_str = formatDimension(value_units).toStdString();
-    auto metric_str = formatDimension(value_units / metricConversationFactor, 1, "m").toStdString();
+    auto units_str = formatDimension(value_units, unitsMaxDigits).toStdString();
+    auto metric_str = formatDimension(value_units / metricConversationFactor, unitsMaxDigits, "m").toStdString();
 
     switch (unitsDisplayType) {
         case Preferences::UNITS:
@@ -391,15 +378,18 @@ void SelectionBoundsRenderer::renderMinMax(RenderContext &renderContext, RenderB
 
     RenderService renderService(renderContext, renderBatch);
     renderService.setForegroundColor(pref(Preferences::InfoOverlayTextColor));
-    renderService.setBackgroundColor(Color(pref(Preferences::InfoOverlayBackgroundColor), pref(Preferences::WeakInfoOverlayBackgroundAlpha)));
+    renderService.setBackgroundColor(
+        Color(pref(Preferences::InfoOverlayBackgroundColor), pref(Preferences::WeakInfoOverlayBackgroundAlpha)));
     renderService.setShowOccludedObjects();
 
     buffer << "Min: " << vm::correct(m_bounds.min);
-    renderService.renderString(buffer.str(), MinMaxTextAnchor3D(m_bounds, vm::bbox3::Corner::min, renderContext.camera()));
+    renderService.renderString(buffer.str(),
+        MinMaxTextAnchor3D(m_bounds, vm::bbox3::Corner::min, renderContext.camera()));
     buffer.str("");
 
     buffer << "Max: " << vm::correct(m_bounds.max);
-    renderService.renderString(buffer.str(), MinMaxTextAnchor3D(m_bounds, vm::bbox3::Corner::max, renderContext.camera()));
+    renderService.renderString(buffer.str(),
+        MinMaxTextAnchor3D(m_bounds, vm::bbox3::Corner::max, renderContext.camera()));
 }
 
 QString SelectionBoundsRenderer::formatDimension(const float value, const int digits, const QString &suffix) {
@@ -408,8 +398,7 @@ QString SelectionBoundsRenderer::formatDimension(const float value, const int di
 
     if (isInt) {
         str = (new QString)->sprintf("%d%s", (int) value, suffix.toStdString().c_str());
-    }
-    else {
+    } else {
         const char *raw_format_str = "%%.%df%%s";
         auto format_str = (new QString)->sprintf(raw_format_str, digits).toStdString();
         str = (new QString)->sprintf(format_str.c_str(), value, suffix.toStdString().c_str());
