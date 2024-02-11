89ed4efdb4c4fe3d2fd91c607015ef81ffcd729d
Patrick Lindenberg <heapdump@icloud.com>
2024-02-11 19:53:55 +0100

    increase max bounds guide length

--- common/src/Renderer/BoundsGuideRenderer.cpp
+++ common/src/Renderer/BoundsGuideRenderer.cpp
@@ -26,7 +26,7 @@
 
 namespace TrenchBroom {
 namespace Renderer {
-const FloatType BoundsGuideRenderer::SpikeLength = 512.0;
+const FloatType BoundsGuideRenderer::SpikeLength = 1024.0;
 
 BoundsGuideRenderer::BoundsGuideRenderer(std::weak_ptr<View::MapDocument> document) : m_document(document) {
 }
