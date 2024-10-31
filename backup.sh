b2401d22a4750f5b6f1ed970baeaa14c99d639dd
Patrick Lindenberg <heapdump@icloud.com>
2024-10-31 20:37:05 +0100

    Rename CreateSimpleBrushTool to DrawShapeTool

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-DATE=$(date "+%Y-%m%d-%H%M%S-%s")
+DATE=$(date "+%Y%m%d%H%M%S-%s")
 
-tar -czvf ${DATE}_backup_tb.tgz ./common/ ./app/ ./lib/ ./*.*
+tar -czvf ${DATE}_backup_tb.tgz ./common ./app ./lib ./*.sh ./.cl*
