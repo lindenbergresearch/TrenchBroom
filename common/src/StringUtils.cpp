2ac88d310b81865a612a8f3fae5a018c042be470
Patrick Lindenberg <heapdump@icloud.com>
2024-12-18 18:22:12 +0100

    update string utils and exited it with statistics heler classes

---
+++ common/src/StringUtils.cpp
@@ -0,0 +1,266 @@
+/*
+Copyright (C) 2010-2017 Kristian Duske
+
+ This file is part of TrenchBroom.
+
+ TrenchBroom is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ TrenchBroom is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "StringUtils.h"
+
+static const char FILL_CHAR = ' ';
+
+namespace TrenchBroom {
+void StatsTable::addString(const std::string &name, const std::string &value) {
+    addPair(name, value);
+}
+
+void StatsTable::addString(const std::string &name, const QString &value) {
+    addPair(name, value.toStdString());
+
+}
+
+void StatsTable::addInt(const std::string &name, const int value) {
+    addPair(name, std::to_string(value));
+
+}
+
+void StatsTable::addLong(const std::string &name, const long value) {
+    addPair(name, std::to_string(value));
+
+}
+
+void StatsTable::addBool(const std::string &name, const bool value) {
+    addPair(name, value ? "true" : "false");
+}
+
+void StatsTable::addFloat(const std::string &name, const float value, const int n, const std::string &suffix) {
+    const auto format = "%." + std::to_string(n) + "f%s";
+    addPair(name, stringf(format.c_str(), value, suffix.c_str()));
+}
+
+void StatsTable::addDouble(const std::string &name, const double value, const int n, const std::string &suffix) {
+    const auto format = "%." + std::to_string(n) + "f%s";
+    addPair(name, stringf(format.c_str(), value, suffix.c_str()));
+}
+
+void StatsTable::addMem(const std::string &name, const size_t value) {
+    if (value >= 1024) {
+        addPair(name, std::to_string(value / 1024) + "K");
+    } else {
+        addPair(name, std::to_string(value) + "B");
+    }
+}
+
+void StatsTable::addTime(const std::string &name, const double value, const TimeUnit tu) {
+    switch (tu) {
+        case SECONDS:
+            addDouble(name, value * static_cast<double>(tu), 4, "s");
+            break;
+        case MILLIS:
+            addDouble(name, value * static_cast<double>(tu), 2, "ms");
+            break;
+        case MICROS:
+            addDouble(name, value * static_cast<double>(tu), 2, "µs");
+            break;
+        case NANOS:
+            addDouble(name, value * static_cast<double>(tu), 1, "ns");
+            break;
+    }
+}
+
+
+
+std::string StatsTable::render(bool reverse) const {
+    std::string buffer{};
+
+    std::vector<TStringPair> items = m_lines;
+
+    if (reverse) {
+        std::reverse(items.begin(), items.end());
+    } else {
+        buffer += m_title + m_lineSeparatorChar + m_lineSeparatorChar;
+    }
+
+    for (const auto &[fst, snd] : items) {
+        buffer += fst;
+
+        if (fst.empty() || fst == "-") {
+            buffer.append(size_t (m_labelWidth + m_rightPadding+14), '-');
+            buffer += m_lineSeparatorChar;
+            continue;
+        }
+
+        if (m_labelWidth > 0) {
+            buffer.append(m_labelWidth - fst.size(), FILL_CHAR);
+        }
+
+        buffer.append(1, m_assignmentChar);
+
+        if (m_rightPadding > 0) {
+            buffer.append(m_rightPadding, FILL_CHAR);
+        }
+
+        buffer += snd;
+        buffer += m_lineSeparatorChar;
+    }
+
+    if (reverse) {
+        buffer += m_lineSeparatorChar + m_title;
+    }
+
+    return buffer;
+}
+
+size_t StatsTable::labelWidth() const {
+    return m_labelWidth;
+}
+
+void StatsTable::setLabelWidth(size_t mLeftSize) {
+    m_labelWidth = mLeftSize;
+}
+
+size_t StatsTable::rightPadding() const {
+    return m_rightPadding;
+}
+
+void StatsTable::setRightPadding(size_t mRightSize) {
+    m_rightPadding = mRightSize;
+}
+
+const std::string &StatsTable::title() const {
+    return m_title;
+}
+
+void StatsTable::setTitle(const std::string &mTitle) {
+    m_title = mTitle;
+}
+
+char StatsTable::assignmentChar() const {
+    return m_assignmentChar;
+}
+
+void StatsTable::setAssignmentChar(char assignmentChar) {
+    m_assignmentChar = assignmentChar;
+}
+
+char StatsTable::lineSeparatorChar() const {
+    return m_lineSeparatorChar;
+}
+
+void StatsTable::setLineSeparatorChar(char lineSeparatorChar) {
+    m_lineSeparatorChar = lineSeparatorChar;
+}
+
+void StatsTable::addPair(std::string name, std::string value, bool pushFront) {
+    if (pushFront) {
+        m_lines.insert(m_lines.begin(), std::pair(name, value));
+    } else {
+        m_lines.push_back(std::pair(name, value));
+    }
+}
+
+void StatsTable::addSeparator() {
+    m_lines.push_back(std::pair("-", ""));
+}
+
+void StatsTable::reset() {
+    m_lines.clear();
+}
+
+Stats::Stats() {
+    reset();
+    min = 10E10;
+    max = 0.0;
+    j = 0;
+}
+
+void Stats::push(double value) {
+    stat.add(value);
+
+    if (value < min) {
+        min = value;
+    }
+
+    if (value > max) {
+        max = value;
+    }
+
+    if (j > resetMinMax) {
+        min = 10E10;
+        max = 0.0;
+        j = 0;
+    }
+}
+
+double Stats::average() {
+    return stat.average();
+}
+
+void Stats::reset() {
+    j = 0;
+    min = 0.0;
+    max = 0.0;
+}
+
+void Stats::softReset() {
+    j++;
+}
+
+std::string replaceAllChars(std::string str, char ch) {
+    std::fill(str.begin(), str.end(), ch);
+    return str;
+}
+
+std::string stringf(const char *const zcFormat, ...) {
+    // initialize use of the variable argument array
+    va_list vaArgs;
+    va_start(vaArgs, zcFormat);
+
+    // reliably acquire the size
+    // from a copy of the variable argument array
+    // and a functionally reliable call to mock the formatting
+    va_list vaArgsCopy;
+    va_copy(vaArgsCopy, vaArgs);
+    const int iLen = std::vsnprintf(nullptr, 0, zcFormat, vaArgsCopy);
+    va_end(vaArgsCopy);
+
+    // return a formatted string without risking memory mismanagement
+    // and without assuming any compiler or platform specific behavior
+    std::vector<char> zc(uint32_t(iLen) + 1UL);
+    std::vsnprintf(zc.data(), zc.size(), zcFormat, vaArgs);
+    va_end(vaArgs);
+
+    return std::string(zc.data(), uint32_t(iLen));
+}
+
+BoxStats::BoxStats(size_t size) : m_pos(0), m_length(static_cast<size_t>(pow(2, size))), m_mask(m_length - 1) {
+    m_data.reserve(m_length);
+}
+
+void BoxStats::add(double n) {
+    m_data[m_pos] = n;
+    m_pos = ++m_pos & m_mask;
+}
+
+double BoxStats::average() {
+    double sum = 0;
+    for (size_t i = 0; i < m_length; i++) {
+        sum += m_data[i];
+    }
+
+    return sum / (double) m_length;
+}
+}
--- common/src/StringUtils.h
+++ common/src/StringUtils.h
@@ -3,7 +3,6 @@
 #include "string"
 
 namespace TrenchBroom {
-
 /**
  * @brief Replacement function of 'stringf' from v0.6.x
  * @param zcFormat Format string
@@ -18,25 +17,126 @@ namespace TrenchBroom {
  * https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
  *
  */
-inline std::string stringf(const char *const zcFormat, ...) {
-    // initialize use of the variable argument array
-    va_list vaArgs;
-    va_start(vaArgs, zcFormat);
+inline std::string stringf(const char *const zcFormat, ...);
 
-    // reliably acquire the size
-    // from a copy of the variable argument array
-    // and a functionally reliable call to mock the formatting
-    va_list vaArgsCopy;
-    va_copy(vaArgsCopy, vaArgs);
-    const int iLen = std::vsnprintf(nullptr, 0, zcFormat, vaArgsCopy);
-    va_end(vaArgsCopy);
+typedef std::pair<std::string, std::string> TStringPair;
 
-    // return a formatted string without risking memory mismanagement
-    // and without assuming any compiler or platform specific behavior
-    std::vector<char> zc(uint32_t(iLen) + 1UL);
-    std::vsnprintf(zc.data(), zc.size(), zcFormat, vaArgs);
-    va_end(vaArgs);
 
-    return std::string(zc.data(), uint32_t(iLen));
-}
+/**
+ * Simple Key-Value Table supporting different datatypes and layout-options.
+ */
+class StatsTable {
+    // the title - leave empty to disable
+    std::string m_title = "";
+    // data buffer
+    std::vector<TStringPair> m_lines;
+    // total width of the label on the left side, 0 means no layout
+    size_t m_labelWidth = 25;
+    // the number of digits after the assignment char
+    size_t m_rightPadding = 1;
+    // assignment char
+    char m_assignmentChar = ':';
+    // end of line char
+    char m_lineSeparatorChar = '\n';
+
+  public:
+    enum TimeUnit {
+      SECONDS,
+      MILLIS = 1000,
+      MICROS = 1000 * MILLIS,
+      NANOS = 1000 * MICROS
+    };
+
+
+    void addPair(std::string name, std::string value, bool pushFront = false);
+
+    void addString(const std::string &name, const std::string &value);
+
+    void addString(const std::string &name, const QString &value);
+
+    void addInt(const std::string &name, const int value);
+
+    void addLong(const std::string &name, const long value);
+
+    void addBool(const std::string &name, const bool value);
+
+    void addFloat(const std::string &name, const float value, const int n = 2, const std::string &suffix = "");
+
+    void addDouble(const std::string &name, const double value, const int n = 2, const std::string &suffix = "");
+
+    void addTime(const std::string &name, const double value, const TimeUnit tu = SECONDS);
+
+    void addMem(const std::string &name, const size_t value);
+
+    void addSeparator();
+
+    std::string render(bool reverse = false) const;
+
+    void reset();
+
+    size_t labelWidth() const;
+
+    void setLabelWidth(size_t mLeftSize);
+
+    size_t rightPadding() const;
+
+    void setRightPadding(size_t mRightSize);
+
+    const std::string &title() const;
+
+    void setTitle(const std::string &mTitle);
+
+    char assignmentChar() const;
+
+    void setAssignmentChar(char assignmentChar);
+
+    char lineSeparatorChar() const;
+
+    void setLineSeparatorChar(char lineSeparatorChar);
+};
+
+
+class BoxStats {
+  private:
+    std::vector<double> m_data{0};
+
+    size_t m_pos;
+    const size_t m_length;
+    const size_t m_mask;
+  public:
+    BoxStats(size_t size = 7);
+
+    void add(double n);
+
+
+    double average();
+
+};
+
+
+struct Stats {
+  double max, min;
+  size_t j, resetMinMax = 44;
+  BoxStats stat{7};
+
+  explicit Stats();
+
+  void push(double value);
+
+  double average();
+
+  void softReset();
+
+  void reset();
+};
+
+/**
+ * @brief Replaces all characters in a string with the specified character.
+ *
+ * @param str The string in which characters will be replaced.
+ * @param ch The character to replace each character in the string with.
+ * @return std::string The modified string with all characters replaced.
+ */
+std::string replaceAllChars(std::string str, char ch);
+
 } // namespace TrenchBroom
